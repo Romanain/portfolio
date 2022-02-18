a96a26bf8414e992d13c551fbb8c6ecc43317a8e
Romanain <rtoebosch@gmail.com>
2022-02-18 18:24:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          This is where the my portfolio will come
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
